cdf3d5bc0485bd8096a2753a2a2313a82287808d
Tharanga Madurapperuma <tharanga.s.madurapperuma@gmail.com>
2024-10-31 19:06:34 +0530

    Create Basic Structure and Routings

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <div>
-          App
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/search" element={<Search />} />
+              <Route path="/createPost" element={<CreatePost />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/signin" element={<SignIn />} />
+              <Route path="/signup" element={<SignUp />} />
+              <Route path="/editProfile" element={<EditProfile />} />
+            </Routes>
+          </BrowserRouter>
         </div>
       </header>
     </div>
