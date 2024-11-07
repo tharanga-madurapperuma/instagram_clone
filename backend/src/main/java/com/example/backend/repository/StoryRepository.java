741e2961846af27a420124ca0db87b92b648be31
Tharanga Madurapperuma <tharanga.s.madurapperuma@gmail.com>
2024-11-07 09:51:16 +0530

    Seperate frontend and backend into different repositories

--- backend/src/main/java/com/example/backend/repository/StoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.backend.repository;
-
-import com.example.backend.model.Story;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface StoryRepository extends MongoRepository<Story, String> {
-}
